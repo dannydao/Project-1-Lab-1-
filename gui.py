bbc3fc46555a052ccd1e4205cb6d8688762a1472
dannydao <dannyhoangdao@gmail.com>
2023-12-05 17:40:48 -0600

    changed to class-based structure and added some input validation

--- gui.py
+++ gui.py
@@ -1,49 +1,97 @@
 import tkinter as tk
-from voting import record_vote, finish_vote
+from voting import VotingFunctions
 
-def record_vote_gui() -> None:
+class VotingAppGUI:
+    def __init__(self, root: tk.Tk) -> None:
+        """
+        Initialize the VotingAppGUI class.
+
+        Args:
+            root: Tkinter root window
+        """
+        self.root = root
+        self.voting_functions = VotingFunctions()
+
+        self.setup_gui()
+
+    def setup_gui(self) -> None:
+        """
+        Sets up the GUI
+        """
+        self.root.title('Voting Application')
+        self.root.geometry('300x250')
+        self.root.resizable(False, False)
+
+        self.var = tk.StringVar()
+        self.var.set(None)
+
+        self.create_widgets()
+
+    def create_widgets(self) -> None:
+        """
+        Creates the GUI widgets
+        """
+        name_label = tk.Label(self.root, text='Enter your name: ')
+        name_label.pack(pady=(10, 5), padx=10, anchor='w')
+
+        self.name_entry = tk.Entry(self.root)
+        self.name_entry.pack(pady=5, padx=10, ipadx=50)
+
+        vote_label = tk.Label(self.root, text='Select your vote: ')
+        vote_label.pack(pady=5, padx=10, anchor='w')
+
+        vote_frame = tk.Frame(self.root)
+        vote_frame.pack(pady=5)
+
+        vote_button1 = tk.Radiobutton(self.root, text='John', variable=self.var, value='John')
+        vote_button1.place(relx=0.43, rely=0.28)
+
+        vote_button2 = tk.Radiobutton(self.root, text='Jane', variable=self.var, value='Jane')
+        vote_button2.place(relx=0.63, rely=0.28)
+
+        record_button = tk.Button(self.root, text='Record vote', command=self.record_vote_gui)
+        record_button.pack(pady=10)
+
+        finish_button = tk.Button(self.root, text='Finish', command=self.finish_vote_gui)
+        finish_button.pack(pady=10)
+
+        self.error_label = tk.Label(self.root)
+        self.error_label.place(relx=0.5, rely=.95, anchor='s')
+
+    def record_vote_gui(self) -> None:
+        """
+        Retrieves voter name and vote. Validates that the name is a string and that the 
+        vote is clicked or else it will tell the voter to select a candidate.
+        """
+        name = self.name_entry.get()
+
+        if not name.replace(' ', '').isalpha():
+            self.error_label.config(text='Please enter a valid name.')
+            return
+    
+        self.error_label.config(text='')
+
+        vote = self.var.get()
+
+        if vote and vote != "None":
+            self.voting_functions.record_vote(name, vote)
+        elif vote == "None":
+            self.error_label.config(text='Please select a candidate.')
+
+    def finish_vote_gui(self):
+        """
+        Finish voting and close the GUI.
+        """
+        self.voting_functions.finish_vote(self.root)
+
+def main() -> None:
     """
-    Retrieves the name and vote from the GUI components and records the vote.
+    Main function to initialize the app.
     """
-    name = name_entry.get()
-    vote = var.get()
-    record_vote(name, vote)
+    root = tk.Tk()
+    VotingAppGUI(root)
+    root.mainloop()
+    
 
-# Main Window creation
-root = tk.Tk()
-root.title("Voting Application")
-
-root.geometry('250x200')
-
-root.resizable(False, False)
-
-# Creating variable to hold the vote choice
-var = tk.StringVar()
-# Setting the default value to John
-var.set('John')
-
-# GUI Elements
-name_label = tk.Label(root, text='Enter your name: ')
-name_label.pack()
-
-name_entry = tk.Entry(root)
-name_entry.pack()
-
-vote_label = tk.Label(root, text='Select your vote: ')
-vote_label.pack()
-
-vote_button1 = tk.Radiobutton(root, text='John', variable=var, value='John')
-vote_button1.pack()
-
-vote_button2 = tk.Radiobutton(root, text='Jane', variable=var, value='Jane')
-vote_button2.pack()
-
-record_button = tk.Button(root, text='Record vote', command= record_vote_gui)
-record_button.pack(pady= 10)
-
-finish_button = tk.Button(root, text='Finish', command=finish_vote)
-finish_button.pack(pady=10)
-
-# Run main event loop
-root.mainloop()
-
+if __name__ == "__main__":
+    main()
--- voting.py
+++ voting.py
@@ -1,58 +1,69 @@
+import tkinter as tk
+from tkinter import messagebox
 import csv
 import os
 import sys
 
-# Function to record vote in a CSV file.
-def record_vote(name: str, vote: str) -> None:
-    """
-    Records voter name and their vote.
+class VotingFunctions:
 
-    Args:
-        name: Voter name
-        vote: Candidate chosen (John or Jane)
-    """
-    # Check if the file exists to prevent rewriting the header
-    write_header = not os.path.exists('vote_results.csv')
+    def record_vote(self, name: str, vote: str) -> None:
+        """
+        Records voter name and their vote.
 
-    with open('vote_results.csv', 'a', newline='') as file:
-        writer = csv.writer(file)
-        if write_header:
-            writer.writerow(['Voter Name', 'Candidate'])
+        Args:
+            name: Voter name
+            vote: Candidate chosen (John or Jane)
+        """
+        # Check if the file exists to prevent rewriting the header
+        write_header = not os.path.exists('vote_results.csv')
 
-        writer.writerow([name, vote])
+        with open('vote_results.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            if write_header:
+                writer.writerow(['Voter Name', 'Candidate'])
 
-# Function to calculate and summarize the votes, then exit the application.
-def finish_vote() -> None:
-    """
-    Finish the voting process, creates summary file.
-    """
-    with open('vote_results.csv', 'r') as file:
-        reader = csv.reader(file)
-        votes = list(reader)
+            writer.writerow([name, vote])
+
+    def finish_vote(self, root: tk.Tk) -> None:
+        """
+        Finish the voting process, creates summary file.
+
+        Args:
+            root: Tkinter root window
+        """
+        if not os.path.exists('vote_results.csv'):
+            # Pop up message if no voting data is found
+            messagebox.showinfo('No Voting Data','No voting data found.')
+            root.destroy()
+            return
+    
+        with open('vote_results.csv', 'r') as file:
+            reader = csv.reader(file)
+            votes = list(reader)
 
         # Exclude header row from count
-        total_votes = len(votes) -1 if votes else 0
+            total_votes = len(votes) -1 if votes else 0
 
         # Count votes for John and Jane as well as total votes
-        person1_votes = sum(1 for row in votes if row[1] == 'John')
-        person2_votes = sum(1 for row in votes if row[1] == 'Jane')
+            person1_votes = sum(1 for row in votes if row[1] == 'John')
+            person2_votes = sum(1 for row in votes if row[1] == 'Jane')
 
         # Writing the summary to a new CSV file.
-        with open('vote_summary.csv', 'w', newline='') as summary_file:
-            writer = csv.writer(summary_file)
-            writer.writerow(['Candidate ', 'Total Votes'])
-            writer.writerow(['John ', person1_votes])
-            writer.writerow(['Jane ', person2_votes])
-            writer.writerow(['Total Votes ', total_votes])
+            with open('vote_summary.csv', 'w', newline='') as summary_file:
+                writer = csv.writer(summary_file)
+                writer.writerow(['Candidate ', 'Total Votes'])
+                writer.writerow(['John ', person1_votes])
+                writer.writerow(['Jane ', person2_votes])
+                writer.writerow(['Total Votes ', total_votes])
     
-    # Clean up and remove the individual votes file
-    if os.path.exists('vote_results.csv'):
-        """
-        Deletes the results
-        """
-        os.remove('vote_results.csv')
+        # Clean up and remove the individual votes file
+        if os.path.exists('vote_results.csv'):
+            """
+            Deletes the results
+            """
+            os.remove('vote_results.csv')
 
-    # Exiting the application
-    sys.exit()
+        # Exiting the application
+        sys.exit()
 
 
